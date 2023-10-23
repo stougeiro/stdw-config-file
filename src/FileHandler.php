ef776a82506361bf363502ad01cbb8794cbc0ff0
S.Tougeiro <sidneytougeiro@gmail.com>
2023-10-23 12:37:46 -0300

    First commit

---
+++ src/FileHandler.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+    namespace STDW\Config\File;
+
+    use STDW\Contract\ConfigHandlerInterface;
+    use STDW\Config\Exception\ItemNotFoundException;
+    use STDW\Config\File\FileException\StorageNotFoundException;
+
+
+    class FileHandler extends ConfigHandlerInterface
+    {
+        private static array $collection = [];
+
+
+        public function __construct()
+        { }
+
+
+        public function get(string $item): mixed
+        {
+            $segments = explode('.', $item);
+            $item = self::$collection;
+
+            foreach ($segments as $segment) {
+                $item = &$item[$segment];
+
+                if ( ! isset($item)) {
+                    throw new ItemNotFoundException("Config: '". implode('.', $segments) ."' do not exists.");
+                }
+            }
+
+            return $item;
+        }
+
+        public function load(string $storage): void
+        {
+            if ( ! file_exists($storage)) {
+                throw new StorageNotFoundException("Config: Storage '$storage' do not exists.");
+            }
+
+            $files = glob($storage. DIRECTORY_SEPARATOR .'*');
+
+            foreach ($files as $file) {
+                $filename = pathinfo($file, PATHINFO_FILENAME);
+                self::$collection[$filename] = include $file;
+            }
+        }
+    }
